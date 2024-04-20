945cdda176a364e591813bd46cf03312341f2c5b
adnanbot1 <102922097+adnanbot1@users.noreply.github.com>
2024-04-20 13:46:06 +0300

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -1,58 +1,14 @@
+import fetch from "node-fetch";
 
-import axios from  axios ;
-import cheerio from  cheerio ;
+let handler = async (m, { conn, usedPrefix, command, text }) => {
+  if (!text) throw 'تحدث مع الذكاء الإصطناعي بواصطة هاذا الأمر \n\n .تحدث ماهي عاصمه اليمن'
+  let anu = await fetch(`https://aemt.me/openai?text=${text}`)
+  let data = await anu.json()
+  m.reply(data.result)
+  }
+handler.help = ['تحدث']
+handler.tags = ['تحدث']
+handler.command = /^(تحدث|openai|gpt)$/i
+handler.limit = false
 
-let handler = async (m, {
-    conn,
-    args,
-    usedPrefix,
-    command
-}) => {
-    let text
-    if (args.length >= 1) {
-        text = args.slice(0).join(" ")
-    } else if (m.quoted && m.quoted.text) {
-        text = m.quoted.text
-    } else throw "مثال \n .تحدث ماهي عاصمه اليمن"
-    await m.reply(waitt)
-    try {
-        let result = await generate(text)
-        await m.reply(result)
-    } catch (e) {
-        await m.reply(eror)
-    }
-}
-handler.help = ["تحدث"]
-handler.tags = ["تحدث"];
-handler.command = /^(تحدث)$/i
 export default handler
-
-/* New Line */
-async function generate(q) {
-    try {
-        const nonceValue = JSON.parse(cheerio.load(await (await axios.get(
-            "https://gpt4free.io/chat"
-        )).data)( .mwai-chatbot-container ).attr( data-system )).restNonce;
-
-        const {
-            data
-        } = await axios(
-            "https://gpt4free.io/wp-json/mwai-ui/v1/chats/submit", {
-                method: "post",
-                data: {
-                    botId: "default",
-                    newMessage: q,
-                    stream: false,
-                },
-                headers: {
-                    "X-WP-Nonce": nonceValue,
-                    "Content-Type": "application/json",
-                },
-            }
-        );
-        return data.reply;
-    } catch (err) {
-        console.log(err.response.data);
-        return err.response.data.message;
-    }
-}
