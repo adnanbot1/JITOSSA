1bec64f815ff18806f3b7197914da10efdc7ebbc
adnanbot1 <102922097+adnanbot1@users.noreply.github.com>
2024-04-22 10:22:28 +0300

    Update apk.js

--- plugins/apk.js
+++ plugins/apk.js
@@ -4,7 +4,7 @@ const handler = async (m, {conn, usedPrefix: prefix, command, text}) => {
   try {    
     const searchA = await search(text);
     const data5 = await download(searchA[0].id);
-    let response = `*اسم التطبيق:* ${data5.name}\n  *أخر تحديث:* ${data5.lastup}\n *حجم التطبيق:* ${data5.size}\n\nقم بمتابعة صاحب البوت لكي تتمكن من الإستفادة من جميع البوتات والمزاية 🫵🏻 \ninstagram.com/ovmar_1`
+    let response = `*اسم التطبيق:* ${data5.name}\n  *أخر تحديث:* ${data5.lastup}\n *حجم التطبيق:* ${data5.size}\n\nقم بمتابعة صاحب البوت لكي تتمكن من الإستفادة من جميع البوتات والمزاية 🫵🏻 \https://wa.me/+967735339177`
     await conn.sendMessage(m.chat, {image: {url: data5.icon}, caption: response}, {quoted: m});
  if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
       return await conn.sendMessage(m.chat, {text: '*[ 😁 ]الملف كبير جدًا لذا لن يتم إرساله.'}, {quoted: m});
@@ -14,7 +14,7 @@ const handler = async (m, {conn, usedPrefix: prefix, command, text}) => {
     throw `*[😒] خطأ، لم يتم العثور على نتائج لبحثك.*`;
   }    
 };
-handler.help = ["apk"]
+handler.help = ["تطبيق"]
 handler.tags = ["applications"]
-handler.command = ["apk"] 
+handler.command = ["تطبيق"] 
 export default handler;
