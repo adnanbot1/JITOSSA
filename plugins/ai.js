81c4c011743fc234b8aa035d95b39ad17d793b11
adnanbot1 <102922097+adnanbot1@users.noreply.github.com>
2024-04-28 02:04:31 +0300

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -1,14 +1,14 @@
 import fetch from "node-fetch";
 
 let handler = async (m, { conn, usedPrefix, command, text }) => {
-  if (!text) throw 'تحدث مع الذكاء الإصطناعي بواصطة هاذا الأمر \n\n .تحدث ماهي عاصمه اليمن'
+  if (!text) throw 'تحدث مع الذكاء الإصطناعي بواصطة هاذا الأمر \n\n .ai عاصمة المغرب ؟'
   let anu = await fetch(`https://aemt.me/openai?text=${text}`)
   let data = await anu.json()
   m.reply(data.result)
   }
-handler.help = ['تحدث']
-handler.tags = ['تحدث']
-handler.command = /^(تحدث|openai|gpt)$/i
+handler.help = ['بوت']
+handler.tags = ['بوت']
+handler.command = /^(بوت|openai|تحدث)$/i
 handler.limit = false
 
 export default handler
