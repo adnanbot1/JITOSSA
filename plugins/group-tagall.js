4dedca8a32d592f327fbbe24b5282edd59ccf5bb
adnanbot1 <102922097+adnanbot1@users.noreply.github.com>
2024-04-28 12:23:49 +0300

    Update group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -1,23 +1,15 @@
-let handler = async (m, { conn, text, participants }) => {
-	let q = m.quoted ? m.quoted : m
-	let mime = (q.msg || q).mimetype || q.mediaType || q.mtype || ''
-	let teks = `⋙ *Message from Admin Group* ⋘ \n\n${text ? text : m.quoted?.text ? m.quoted.text : m.quoted?.caption ? m.quoted.caption : m.quoted?.description ? m.quoted.description : 'Nothing'}\n\n`
-	teks += `┌─\n`
-	for (let mem of participants) {
-		teks += `│◦◈ @${mem.id.split('@')[0]}\n`
-	}
-	teks += `└────`
-	if (/video|image|viewOnce/g.test(mime) && !/webp/g.test(mime)) {
-		let media = await q.download?.()
-		await conn.sendFile(m.chat, media, '', teks, null, false, { mentions: participants.map(a => a.id), quoted: m })
-	} else await conn.reply(m.chat, teks, fkontak, { mentions: participants.map(a => a.id) })
+let handler = async(m, { conn, text, participants }) => {
+  let teks = `${text ? text :    }\n\n`
+		      	for (let mem of participants) {
+		            teks += `@${mem.id.split( @ )[0]}\n`
+				}
+                conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
-
-handler.help = ['منشن']
-handler.tags = ['group']
+handler.help = [ منشن <pesan> ]
+handler.tags = [ group ]
 handler.command = /^(منشن)$/i
 
-handler.admin = true
 handler.group = true
+handler.admin = true
 
-export default handler
+module.exports = handler
