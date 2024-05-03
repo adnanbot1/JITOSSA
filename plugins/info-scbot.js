11886780d9030b338933c293cb5dfa1c267c0188
adnanbot1 <102922097+adnanbot1@users.noreply.github.com>
2024-05-03 20:10:48 +0300

    Update info-scbot.js

--- plugins/info-scbot.js
+++ plugins/info-scbot.js
@@ -1,24 +1,24 @@
-import { promises } from 'fs'
-import { join } from 'path'
+import { promises } from  fs 
+import { join } from  path 
 
 let handler = async function (m, { conn, __dirname }) {
-let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
+let _package = JSON.parse(await promises.readFile(join(__dirname,  ../package.json )).catch(_ => ({}))) || {}
   
 m.reply(`  
-*≡ السكريبت: JITOSSA*  
-*JITOSSA هو بوت وسكريبت API يتخصص في إرسال التطبيقات والأغاني ومقاطع الفيديو وغيرها من المحتويات المتعلقة بالهاتف وتطبيقاته.*  
+*≡ السكريبت: بوت لوسيفار*  
+*بوت لوسيفار هو بوت وسكريبت API يتخصص في إرسال التطبيقات والأغاني ومقاطع الفيديو وغيرها من المحتويات المتعلقة بالهاتف وتطبيقاته.*  
 
 *▢ يمكنك الحصول على السكريبت من هنا:* 
 
-*[رابط السكريب]*
-https://www.github.com/Omarcharaf1/JITOSSA
+*[ليس هناك سكربت تحت التعديل يمكنك مراسله رقم المطور]*
+https://wa.me/+967775317680
 
 `.trim())
     
 }
 
-handler.help = ['script']
-handler.tags = ['tools']
-handler.command = ['sc', 'git', 'script'] 
+handler.help = [ المطور ]
+handler.tags = [ tools ]
+handler.command = [ الدعم ,  المطور ,  المطور ] 
 
 export default handler
