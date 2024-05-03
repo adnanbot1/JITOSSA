8f51e94b0843c05d90c484db081ab9d566e826d2
adnanbot1 <102922097+adnanbot1@users.noreply.github.com>
2024-05-03 03:22:53 +0300

    Update دحيح.js

--- plugins/دحيح.js
+++ plugins/دحيح.js
@@ -1,1 +1,34 @@
-const _0x121d41=_0xbebd;(function(_0xb70086,_0x560a43){const _0x607f59=_0xbebd,_0x55afd1=_0xb70086();while(!![]){try{const _0x2e949d=parseInt(_0x607f59(0x1d5))/0x1*(parseInt(_0x607f59(0x1d3))/0x2)+-parseInt(_0x607f59(0x1d6))/0x3+-parseInt(_0x607f59(0x1cd))/0x4+parseInt(_0x607f59(0x1e1))/0x5+-parseInt(_0x607f59(0x1f3))/0x6*(parseInt(_0x607f59(0x1f1))/0x7)+-parseInt(_0x607f59(0x1e5))/0x8*(-parseInt(_0x607f59(0x1cf))/0x9)+parseInt(_0x607f59(0x1e9))/0xa*(-parseInt(_0x607f59(0x1e2))/0xb);if(_0x2e949d===_0x560a43)break;else _0x55afd1['push'](_0x55afd1['shift']());}catch(_0x3e81ac){_0x55afd1['push'](_0x55afd1['shift']());}}}(_0x4ced,0xde608));import _0x3ec5fb from'node-fetch';import _0xdcffbe from'../lib/uploadImage.js';let apiurl=_0x121d41(0x1cc),apiurl4=_0x121d41(0x1ec),apiurl6='ef66b9',apiurl7=_0x121d41(0x1d9),apiurl2=_0x121d41(0x1da),apiurl3=_0x121d41(0x1dc),apiurl5=_0x121d41(0x1e6),handler=async(_0x4f9075,{text:_0x378db0,conn:_0x5f1265,usedPrefix:_0x8563f,command:_0x4d8ac9})=>{const _0xec2410=_0x121d41;if(!_0x378db0&&!(_0x4f9075[_0xec2410(0x1e4)]&&_0x4f9075['quoted'][_0xec2410(0x1f0)]))throw _0xec2410(0x1f2)+(_0x8563f+_0x4d8ac9)+_0xec2410(0x1d2);try{const _0x1429fb=encodeURIComponent(_0x378db0);let _0x57eaa9=null,_0x30b50b='',_0x4e23aa=_0x4f9075['quoted']?_0x4f9075['quoted']:_0x4f9075;if((_0x4e23aa[_0xec2410(0x1cb)]||_0x4e23aa)[_0xec2410(0x1e8)]||_0x4e23aa['mediaType']||''){let _0x2c1b59=(_0x4e23aa[_0xec2410(0x1cb)]||_0x4e23aa)['mimetype']||_0x4e23aa[_0xec2410(0x1ee)]||'';if(_0x2c1b59[_0xec2410(0x1e3)]('video/'))return _0x4f9075[_0xec2410(0x1ed)](_0xec2410(0x1db));_0x57eaa9=await _0x4e23aa['download']();let _0x156836=/image\/(png|jpe?g|gif)/[_0xec2410(0x1f5)](_0x2c1b59);_0x30b50b=await(_0x156836?_0xdcffbe:_0xdcffbe)(_0x57eaa9);}const _0x42a6e7=_0x30b50b?apiurl+apiurl2+apiurl3+apiurl4+apiurl5+apiurl6+apiurl7+_0xec2410(0x1df)+_0x1429fb+_0xec2410(0x1e7)+_0x30b50b:apiurl+apiurl2+apiurl3+apiurl4+apiurl5+apiurl6+apiurl7+'/googlegenai?query='+_0x1429fb+_0xec2410(0x1e7);_0x5f1265[_0xec2410(0x1d1)](_0xec2410(0x1d7),_0x4f9075[_0xec2410(0x1d4)]);const _0x3d1c3b=await _0x3ec5fb(_0x42a6e7),_0x3ef200=await _0x3d1c3b[_0xec2410(0x1f6)](),_0x3d2a84=_0x3ef200[_0xec2410(0x1de)];_0x4f9075['reply'](_0x3d2a84);}catch(_0x392add){console[_0xec2410(0x1d8)](_0xec2410(0x1f4),_0x392add);throw _0xec2410(0x1ce);}};function _0x4ced(){const _0x162498=['json','msg','htt','1251136CiTPnf','*حدث\x20خطأ\x20فيـAPI*','241623QuQuqJ','gemini','sendPresenceUpdate','\x20اكتب\x20رمز\x20JS*\x0a*ملاحظة\x20الأمر\x20هذا\x20يمكنه\x20قرأة\x20الصور\x20أيضا*\x0a','186VYKjZb','chat','4222rfFnjn','537522RztXdG','composing','error','.herokuapp.com','ps://ap','❌\x20يرجى\x20الرد\x20على\x20صورة،\x20لا\x20فيديو!','i-da','جيمي','result','/googlegenai?query=','دحيح','7010420lqJzOs','36773HoLOIb','startsWith','quoted','72jEcrRE','n-3cf8c6','&url=','mimetype','1750IrFpLa','googlegenai','command','rkma','reply','mediaType','tags','text','169799hYNsjR','*أدخل\x20نصًا\x20أو\x20قم\x20بالرد\x20على\x20رسالة\x20بنص\x20لاستخدام\x20Gemini\x20Google*\x0a\x0a*مثال:*\x0a*','12KljZje','Error:','test'];_0x4ced=function(){return _0x162498;};return _0x4ced();}handler['help']=[_0x121d41(0x1ea)],handler[_0x121d41(0x1ef)]=['AI'],handler[_0x121d41(0x1eb)]=[_0x121d41(0x1e0),_0x121d41(0x1ea),_0x121d41(0x1d0),'جيميناي',_0x121d41(0x1dd)];function _0xbebd(_0x37e7e3,_0x507400){const _0x4ced98=_0x4ced();return _0xbebd=function(_0xbebd98,_0x5b6be9){_0xbebd98=_0xbebd98-0x1cb;let _0x490d77=_0x4ced98[_0xbebd98];return _0x490d77;},_0xbebd(_0x37e7e3,_0x507400);}export default handler;
+const uploadImage = require("../lib/uploadFile")
+let handler = async (m, {
+	conn,
+	args,
+	usedPrefix,
+	command
+}) => {
+	let text
+	if (args.length >= 1) {
+		text = args.slice(0).join(" ")
+	} else if (m.quoted && m.quoted.text) {
+		text = m.quoted.text
+	} else return m.reply("• *Example:* .gemini cara menikmati kelamin")
+	let q = m.quoted ? m.quoted : m
+	let mime = (q.msg || q).mimetype || ""
+	let salsa = await conn.reply(m.chat, '```Sedang mencari jawaban...🔍```', m)
+	if (!mime) {
+			let res = await itz.gemini(text)
+			await await conn.sendMessage(m.chat, { text: `${res.result}`, edit: salsa })
+			
+	} else {
+		let media = await q.download()
+		let isTele = /image\/(png|jpe?g)/.test(mime)
+		let link = await uploadImage(media)
+		let res = await itz.geminiimage(text, link)
+		await await conn.sendMessage(m.chat, { text: `${res.result}`, edit: salsa })
+}
+  }
+handler.help = ["gemini"].map(a => a + " *<text>*")
+handler.tags = ["ai"]
+handler.command = /^(gemini)$/i
+handler.premium = false
+handler.register = true
+module.exports = handler
